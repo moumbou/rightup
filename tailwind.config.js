a15aae21e95562253d7108526c6c2b609ac5611b
BigCat <mbouzidi63@gmail.com>
2022-11-09 05:03:21 +0100

    our work

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-srif"],
+        futura: ["Hind", "sans-srif"],
+      },
+      colors: {
+        "r-white": "#FEF9FA",
+        "r-dark": "#1B2228",
+        "r-main": "#FFFAFB",
+        "r-blue": "#007FFF",
+        "r-yellow": "#FFB830",
+        "r-red": "#FF003D",
+      },
+    },
+  },
+  variants: {
+    display: [
+      "children",
+      "default",
+      "children-first",
+      "children-last",
+      "children-odd",
+      "children-even",
+      "children-not-first",
+      "children-not-last",
+      "children-hover",
+      "hover",
+      "children-focus",
+      "focus",
+      "children-focus-within",
+      "focus-within",
+      "children-active",
+      "active",
+      "children-visited",
+      "visited",
+      "children-disabled",
+      "disabled",
+      "responsive",
+    ],
+  },
+  plugins: [require("tailwindcss-children")],
+};
